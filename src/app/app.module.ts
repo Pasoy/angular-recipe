f314a16c984db0978842c028149575ed74d1b32b
Pasoy <43441885+Pasoy@users.noreply.github.com>
2019-01-29 00:30:25 +0100

    all components, disabled spec files..
    - Added Header, Footer
    - Added Recipe System
    - Added Shopping List System

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './partials/header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { FooterComponent } from './partials/footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
