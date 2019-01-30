4e730f0dbb270339b795c6f0475b596053a63230
Pasoy <43441885+Pasoy@users.noreply.github.com>
2019-01-30 20:14:10 +0100

    list item changed, managing buttons...

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,8 @@ export class RecipeListComponent implements OnInit {
 
   constructor() {
     this.recipes.push(new Recipe('Testy', 'Lawl yoo desc', 'https://cdn.masterani.me/poster/1/1893neuYUyo0.jpg'));
+    this.recipes.push(new Recipe('Testy2', 'Lawl yoo desc2', 'https://cdn.masterani.me/poster/1/226mhyBWAID.jpg'));
+    this.recipes.push(new Recipe('Testy3', 'Lawl yoo desc3', 'https://cdn.masterani.me/poster/1/231a16Y7xZO.jpg'));
   }
 
   ngOnInit() {
